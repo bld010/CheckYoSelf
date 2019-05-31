a3ba7b08396d0899f1a495d2c0bb9e399736fe94
Brianna DelValle <briannadelvalle@gmail.com>
2019-05-31 09:17:23 -0600

    Create HTML and beginning of CSS styling

---
+++ todo-list.js
@@ -0,0 +1,24 @@
+class ToDoList {
+  constructor(id, title, urgent){
+    this.id = id
+    this.title = title
+    this.urgent = urgent || false;
+    this.tasks = [];
+  }
+
+  saveToStorage(){
+
+  }
+
+  deleteFromStorage(){
+
+  }
+
+  updateToDo(){
+
+  }
+
+  updateTask(){
+
+  }
+}
